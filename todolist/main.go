f60f7806d06f40087319b967859297cb686e8410
Valdy Ramadhan <110575212+elysiamori@users.noreply.github.com>
2023-11-14 08:31:00 +0700

    Update main.go

--- todolist/main.go
+++ todolist/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"github.com/elysiamori/finalproject1/kelompok6/config"
 	"github.com/elysiamori/finalproject1/kelompok6/routers"
 )
 
 func main() {
+	_, err := config.DBConn()
+
+	if err != nil {
+		panic(err)
+	}
+
 	r := routers.StartRouter()
 	r.Run(":3000")
 }
