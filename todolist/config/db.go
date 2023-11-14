b45fec11f43648f1fc98489468cbd183b2360a4e
Valdy Ramadhan <110575212+elysiamori@users.noreply.github.com>
2023-11-14 08:31:35 +0700

    Update db.go

--- todolist/config/db.go
+++ todolist/config/db.go
@@ -1,22 +1,48 @@
 package config
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/elysiamori/finalproject1/kelompok6/models"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-// DBConn
-func DBConn() *gorm.DB {
-	conn := "postgres://postgres:postgres@localhost:5432/todos?sslmode=disable"
+type ConfigDB struct {
+	host     string
+	port     string
+	user     string
+	password string
+	dbname   string
+}
 
+// DBConn
+func DBConn() (*gorm.DB, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Gagal memuat file .env: %v", err)
+	}
+
+	config := ConfigDB{
+		host:     os.Getenv("DB_HOST"),
+		port:     os.Getenv("DB_PORT"),
+		user:     os.Getenv("DB_USER"),
+		password: os.Getenv("DB_PASS"),
+		dbname:   os.Getenv("DB_NAME"),
+	}
+
+	conn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", config.user, config.password, config.host, config.port, config.dbname)
+	
 	db, err := gorm.Open(postgres.Open(conn), &gorm.Config{})
 
 	if err != nil {
-		return nil
+		return nil, err
 	}
 
 	db.AutoMigrate(&models.Todos{})
 
-	return db
+	return db, nil
 }
